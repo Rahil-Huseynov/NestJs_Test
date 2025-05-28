f3b2a694c129ce0be10d2befbe380b23c407e525
Rahil-Huseynov <156452922+Rahil-Huseynov@users.noreply.github.com>
2025-05-28 14:47:42 +0400

    integration Prisma,Argon and Dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,19 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto";
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) { }
 
-    @Post('singup')
+     @Post('singup')
+     singup(@Body() dto:AuthDto) {
+        return this.authService.singup(dto)
+    }
+    
+    
+    @Post('singin')
     singin() {
         return this.authService.singin()
-    }
-
-    @Post('singin')
-    singup() {
-        return this.authService.singup()
     }
 }
