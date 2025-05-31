282d94c80873a42ba388f2a5f20061c28ba045df
Rahil-Huseynov <156452922+Rahil-Huseynov@users.noreply.github.com>
2025-05-31 17:08:24 +0400

    Get current user with access token

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*', // Uygulamanıza erişim izni verilen origin adresi
+    origin: '*',
     methods: 'GET,PUT,POST,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
   });
